2430def797de3ee9cbd8f85586a172bec7e95390
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-05-19 21:18:56 -0300

    Hice dos contenedores que muestra la fecha y la hora

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-const fecha = document.querySelector(".fecha");
-const hora = document.querySelector(".hora");
+function obtenerHorario() {
+    const fecha = new Date();
+    const contenedorHora = document.querySelector(".contenedorHora");
+    const segundos = fecha.getSeconds() < 10 ? "0" + fecha.getSeconds() : fecha.getSeconds();
+    const minutos = fecha.getMinutes() < 10 ? "0" + fecha.getMinutes() : fecha.getMinutes();
+    const horas = fecha.getHours() < 10 ? "0" + fecha.getHours() : fecha.getHours();
+    contenedorHora.textContent = `${horas}: ${minutos}: ${segundos}`;
+}
 
-console.log(fecha);
+function obetenerFecha() {
+    const fecha = new Date;
+    const contenedorFecha = document.querySelector(".contenedorFecha");
+    const dia = fecha.getDate();
+    const mes = fecha.getMonth();
+    const anio = fecha.getFullYear();
+    contenedorFecha.textContent = `${dia}/${mes}/${anio}`;
+}
+
+const contenedorFecha = document.querySelector(".contenedorFecha");
+const contenedorHora = document.querySelector(".contenedorHora");
+
+setInterval(obtenerHorario, 1000);
+obetenerFecha();
