cb6a7c34dcb7c50fe14310107a198ff43cf0dbd2
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-05-19 22:02:23 -0300

    Modifiqué el formato en el que se mostraba la fecha

--- js/script.js
+++ js/script.js
@@ -10,10 +10,37 @@ function obtenerHorario() {
 function obetenerFecha() {
     const fecha = new Date;
     const contenedorFecha = document.querySelector(".contenedorFecha");
+    const diaDeLaSemana = fecha.getDay();
     const dia = fecha.getDate();
-    const mes = fecha.getMonth();
+    const numeroMes = fecha.getMonth();
     const anio = fecha.getFullYear();
-    contenedorFecha.textContent = `${dia}/${mes}/${anio}`;
+    const diasDeLaSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    let mes = "";
+    let nombreDiaDeLaSemana  ="";
+
+    switch (numeroMes) {
+        case 0: mes = "Enero"; break
+        case 1: mes = "Febrero"; break
+        case 2: mes = "Marzo"; break
+        case 3: mes = "Abril"; break
+        case 4: mes = "Mayo"; break
+        case 5: mes = "Junio"; break
+        case 6: mes = "Julio"; break
+        case 7: mes = "Agosto"; break
+        case 8: mes = "Septiembre"; break
+        case 9: mes = "Octubre"; break
+        case 10: mes = "Noviembre"; break
+        case 11: mes = "Diciembre"; break
+    }
+
+    for (let i = 0; i < diasDeLaSemana.length; i++) {
+        if(i === diaDeLaSemana){
+            nombreDiaDeLaSemana = diasDeLaSemana[i];
+        }
+    }
+
+    console.log(mes);
+    contenedorFecha.textContent = `${nombreDiaDeLaSemana} ${dia} de ${mes} del ${anio}`;
 }
 
 const contenedorFecha = document.querySelector(".contenedorFecha");
@@ -21,3 +48,4 @@ const contenedorHora = document.querySelector(".contenedorHora");
 
 setInterval(obtenerHorario, 1000);
 obetenerFecha();
+
